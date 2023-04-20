283676d3a1d38c57b656280a48c98016319c4167
Vinícius Soares <viniciussou222@gmail.com>
2023-04-20 01:08:27 -0300

    InheritanceType.TABLE_PER_CLASS

---
+++ src/modelo/heranca/AlunoBolsista.java
@@ -0,0 +1,37 @@
+package modelo.heranca;
+
+import javax.persistence.Entity;
+
+@Entity
+public class AlunoBolsista extends Aluno {
+	
+	private double valorBolsa;
+	
+	
+
+	public AlunoBolsista() {
+		super();
+	}
+
+
+
+	public AlunoBolsista(Long matricula,String nome,double valorBolsa) {
+		super(matricula, nome);
+		this.valorBolsa = valorBolsa;
+	}
+
+
+
+	public double getValorBolsa() {
+		return valorBolsa;
+	}
+
+
+
+	public void setValorBolsa(double valorBolsa) {
+		this.valorBolsa = valorBolsa;
+	}
+	
+	
+	
+}
